afd2c17003106192a66108e1a733be65feff0440
JoshDaos <joshuadaos1998@gmail.com>
2021-04-25 14:15:43 -0700

    added users contoller file

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+// require express
+const express = require("express");
+// set up router
+const router = express.Router();
+// internal modules (database)
+const db = require("../models");
+
+/* RESTFUL ROUTES
+URL // HTTP // ACTION
+GET - index
+GET - new
+POST - create
+GET - show 
+GET - edit 
+PATCH/PUT - update
+DELETE - destroy
+*/
+
+
+
+
+
+// exports
+module.exports = router;
