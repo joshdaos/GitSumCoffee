08e2c0aec2ed9b89ffe067390a2f5da9ad1bd62d
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-04-26 07:23:36 -0700

    added mongose syntax to index.js/models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
+const mongoose = require("mongoose");
+
+const dbURL = "mongodb://localhost:27017/blogdb";
+
+mongoose
+    .connect(dbURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+
 module.exports = {
     Products: require("./Products")
 };
