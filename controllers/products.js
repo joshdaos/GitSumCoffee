5cd459c8fabe1a4cacce9ee2ecbc39857c6cb9a1
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-04-27 14:18:40 -0700

    added db stuffs

--- controllers/products.js
+++ controllers/products.js
@@ -32,9 +32,8 @@ router.get("/bestsellers", function (request, response) {
 //Show Selected Coffee Route
  router.get("/:id", function(request, response) {
         db.Product.findById(request.params.id)
-        .populate("product");
-        db.Product.findById()
-        .exec(function (err, foundArticle) {
+        .exec(function (err, foundProduct) {
+       
         if (err) return response.send(err);
 
         const context = { product: foundProduct };
