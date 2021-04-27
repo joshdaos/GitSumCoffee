31ffe21f8e2bb49e22222daf949f51ce3f58a31c
JoshDaos <joshuadaos1998@gmail.com>
2021-04-26 21:51:42 -0700

    added post login route in auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 // internal modules (database)
 const db = require("../models");
+const { response } = require("express");
+const { request } = require("../server");
 
 /* RESTFUL ROUTES
 URL // HTTP // ACTION
@@ -23,17 +25,43 @@ router.get("/signup", function (request, response){
     response.render("auth/signup");
 });
 
-router.post("/signup", async function(req,res){
-    res.send(req.body);
-  });
+router.post("/signup", async function(request,response){
+    try {
+    // step check if user exists 
+    const foundUser = await db.User.findOne({email: request.body.email});
+    // if so redirect to login 
+    if(foundUser){
+      return response.redirect("/login");
+    }
+    // if not create user and redirect to login
   
-  router.get("/login",function(req,res){
-    res.render("auth/login");
-  });
+      // salt will created a more complicated hash
+    const salt = await bcrypt.genSalt(10);
+      // hash will convert our password into something more secure
+      // test1234 => "$2a$10$5vR9VhGpkARz6EFPdkuNQ.aZNRGUgSCNSKEb9Xp1IKzrfxYETlkB2"
+    const hash = await bcrypt.hash(request.body.password, salt);
+    
+    request.body.password = hash;
   
-  router.post("/login", async function(req,res){
-    res.send(req.body);
-  });
+      // create user in database
+    const newUser = await db.User.create(req.body);
+    
+    return response.redirect("/login");
+  
+    } catch(err){
+      console.log(err);
+      return response.send(err);
+    }
+});
+
+
+router.get("/login",function(request,response){
+    response.render("auth/login");
+});
+  
+  router.post("/login", async function(request,response){
+    response.send(request.body);
+});
 
 
 
