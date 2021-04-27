bcbc05ef2452a8b65d5ceb081eea688f65b60e11
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-04-26 20:51:31 -0700

    added session middleware to server.js but commented out

--- server.js
+++ server.js
@@ -23,6 +23,48 @@ app.set("view engine", "ejs");
 //middleware//
 app.use(express.urlencoded({ extended: true }));
 
+/*
+/ setup session middleware 
+// session(config object)
+app.use(session({
+	// where to store the sessions in mongodb
+	store: MongoStore.create({ mongoUrl: process.env.MONGO_URI}),
+	// secret key is used to sign every cookie to say its is valid
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	// configure the experation of the cookie
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // two weeks
+	}
+}));
+
+// logger middleware
+// all controller functions take in req,res,next
+app.use(function(req,res,next){
+	console.log(`${req.method} - ${req.url}`);
+	console.log(req.session);
+	// we use next in routes to tell express to move on to the next route in order
+	next();
+});
+
+// add user credientials to ejs files
+app.use(function(req,res,next){
+	app.locals.user = req.session.currentUser;
+	next();
+});
+
+// authRequired middleware
+const authRequired = function(req,res,next){
+	if(req.session.currentUser){
+		return next();
+	}
+
+	return res.redirect("/login");
+};
+
+*/
+
 //controllers//
 app.use("/products", controllers.products);
 app.use("/auth", controllers.auth);
