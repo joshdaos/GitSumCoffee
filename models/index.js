7c4dd52ef18c2382027aaac4c9618fb762b314b4
JoshDaos <joshuadaos1998@gmail.com>
2021-04-27 12:30:26 -0700

    created coffeedb in mongo

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //require('dotenv').config();
 //const dbUrl = process.env.MONGO_URI;
-const dbURL = "mongodb://localhost:27017/blogdb";
+const dbURL = "mongodb://localhost:27017/coffeedb";
 
 mongoose
     .connect(dbURL, {
