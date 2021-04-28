ffa7b3f001834719e63055c5bf4ee0d6832bcc09
JoshDaos <joshuadaos1998@gmail.com>
2021-04-28 09:46:59 -0700

    got helped for admin

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,10 +34,9 @@ router.post("/signup", async function(request,response){
     }
     // if not create user and redirect to login
   
-      // salt will created a more complicated hash
+      // hashes out password
     const salt = await bcrypt.genSalt(10);
-      // hash will convert our password into something more secure
-      // test1234 => "$2a$10$5vR9VhGpkARz6EFPdkuNQ.aZNRGUgSCNSKEb9Xp1IKzrfxYETlkB2"
+     
     const hash = await bcrypt.hash(request.body.password, salt);
     
     request.body.password = hash;
@@ -68,17 +67,14 @@ router.post("/login", async function(request,response){
   
     // if the user exists
       // validate the user if passwords match -> login 
-      // .compare(body password, hashed password) => return true or false
     const match = await bcrypt.compare(request.body.password, foundUser.password);
   
     // if not match send error
     if(!match) return response.send("password invalid");
   
     // if match create the session and redirect to home\
-    // here we have created the key card
     request.session.currentUser = {
       id: foundUser._id,
-    //   username: foundUser.username
     }
     
     return response.redirect("/products/bestsellers");
